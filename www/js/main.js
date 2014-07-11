1b36c2ee381980028e6d5bb4500fbc75cf14609b
Drake Emko <demko@revionics.com>
2014-07-11 13:15:03 -0600

    make coins slightly more rare

--- www/js/main.js
+++ www/js/main.js
@@ -414,7 +414,7 @@ window.onload = function() {
 
 			// Check if it's time to create a new set of coins
 			this.generateCoinTimer += evt.elapsed * 0.001;
-			timeBeforeNext = 9 + Math.floor(Math.random() * 14); // increase to make coins more rare
+			timeBeforeNext = 10 + Math.floor(Math.random() * 14); // increase to make coins more rare
 			if (this.generateCoinTimer >= timeBeforeNext) { 
 				this.generateCoinTimer -= timeBeforeNext;
 				var xpos = leftBorder + 10 + Math.floor(Math.random() * (rightBorder - leftBorder - 10));
