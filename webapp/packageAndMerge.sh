4d50a51ee052e2f7cb35ef5ecaab398a0cdb847c
Drake Emko <drakee@gmail.com>
2017-02-13 21:04:24 -0700

    fixed packageAndMerge.sh to copy files into correct root directory

--- webapp/packageAndMerge.sh
+++ webapp/packageAndMerge.sh
@@ -2,12 +2,12 @@
 zip -r chrome.zip chrome
 
 # clean up working web app directory
-rm -fr www/*
+rm -fr www
 
 # copy web assets from main project's www directory
 cp -av ../www/ www/
 
 # overwrite default web assets with any chrome-specific versions of those files
-cp -av merges/ www/
+cp -av merges/* www/
 
 # everything in the ./www directory is now ready to be uploaded to a hosting site
